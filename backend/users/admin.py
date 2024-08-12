e30acd23fb91e6ecf8005bba414facf9f86ebf39
Levina Maria <lvnmary@yandex.ru>
2024-08-12 14:28:19 +0300

    add admin.py in users

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User, Subscribe
+from .models import Subscribe, User
 
-admin.site.register(User)
-admin.site.register(Subscribe)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'password',
+        'role',
+    )
+    search_fields = ('username',)
+    list_filter = ('username',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following_user',)
+    list_filter = ('user', 'following_user',)
+    empty_value_display = '-пусто-'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
 from .constants import (
