69484ebfc6d0a408335eca5f36b97c89576a1200
Levina Maria <lvnmary@yandex.ru>
2024-08-16 20:16:42 +0300

    add admin for recipes

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.admin import display
 
-# Register your models here.
+from .models import (
+    Tag, Ingredient, Recipe, IngredientsInRecipes, Favorites, ShoppingList
+)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'id',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    list_filter = ('measurement_unit',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'id', 'favorites_count')
+    list_filter = ('name', 'author', 'tags',)
+    search_fields = ('name',)
+    readonly_fields = ('favorites_count',)
+    empty_value_display = '-пусто-'
+
+    @display
+    def favorites_count(self, obj):
+        return obj.favorites.count()
+
+
+@admin.register(IngredientsInRecipes)
+class IngredientsInRecipesAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Favorites)
+class FavoritesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+    empty_value_display = '-пусто-'
