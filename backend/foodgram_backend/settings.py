4c64b5c1acccc922e0b1754033ef1189df14a183
Maria <lvnmary@yandex.ru>
2024-08-23 22:12:42 +0000

    fix5

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -104,15 +104,17 @@ TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = "/static_backend/"
-STATIC_ROOT = BASE_DIR / "static_backend"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "collected_static"
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
