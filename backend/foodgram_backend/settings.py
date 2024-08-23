59ffc3a5d1537d3fe5c26777ff7455c8127b8820
Maria <lvnmary@yandex.ru>
2024-08-23 11:59:33 +0000

    add nginx

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,7 +11,7 @@ SECRET_KEY = "django-insecure-4r3i_)i%bfh7dh!!_=wa61q&yao@h7vx^()9h@14=yrhz3bjo*
 
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # SECRET_KEY = os.getenv("SECRET_KEY", "default")
 
@@ -75,21 +75,14 @@ WSGI_APPLICATION = "foodgram_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "django"),
+        "USER": os.getenv("POSTGRES_USER", "django"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ''),
+        "HOST": os.getenv("DB_HOST", ''),
+        "PORT": os.getenv("DB_PORT", 5432)
     }
 }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("POSTGRES_DB", "django"),
-#         "USER": os.getenv("POSTGRES_USER", "django"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", ''),
-#         "HOST": os.getenv("DB_HOST", ''),
-#         "PORT": os.getenv("DB_PORT", 5432)
-#     }
-# }
 
 # Password validation
 
@@ -122,7 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = "static/"
+STATIC_URL = "/static_backend/"
+STATIC_ROOT = BASE_DIR / "static_backend"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
