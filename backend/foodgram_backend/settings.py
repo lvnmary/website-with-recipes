88de243ec553172931abf28729c883bf1c4730df
Levina Maria <lvnmary@yandex.ru>
2024-09-05 23:13:51 +0300

    fix23

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -145,10 +145,6 @@ REST_FRAMEWORK = {
 
 DJOSER = {
     "LOGIN_FIELD": "email",
-    "SERIALIZERS": {
-        'user': 'api.serializers.UserSerializer',
-        'current_user': 'api.serializers.UserSerializer',
-    },
     "PERMISSIONS": {
         "user": ["rest_framework.permissions.IsAuthenticatedOrReadOnly",],
         "user_list": ["rest_framework.permissions.AllowAny",],
